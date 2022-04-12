912877e22e6c310f75961c7b8e38b345863d73f5
machava2 <machava2@login1.rci.cvut.cz>
2022-04-12 13:42:53 +0200

    Added exports

---
+++ src/Experiments/src/Experiments.jl
@@ -0,0 +1,26 @@
+module Experiments
+
+using AccuracyAtTopPrimal
+using Configurations
+using DrWatson
+using Flux
+using HDF5
+using Random
+using Statistics
+
+using Flux.Losses: logitbinarycrossentropy
+
+export Dataset
+export CrossEntropy
+export PatMatObjective
+export Model
+
+export load
+export materialize
+export loss
+
+include("datasets.jl")
+include("objectives.jl")
+include("models.jl")
+
+end # module
