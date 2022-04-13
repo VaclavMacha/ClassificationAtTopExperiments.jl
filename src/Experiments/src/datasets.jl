1320cd2abb8850fb253821e9b5bb0e50bf5d0a76
machava2 <machava2@login1.rci.cvut.cz>
2022-04-13 12:17:07 +0200

    Change datasets

--- src/Experiments/src/datasets.jl
+++ src/Experiments/src/datasets.jl
@@ -8,11 +8,22 @@ function load_hdf5(path::AbstractString)
     end
 end
 
-@option "dataset" struct Dataset
-    cover_file::String = "cover_jrm.h5"
-    stego_file::String = "nsf5_0.1_jrm_partial.h5"
-    cover_ratio::Float64 = 1
-    stego_ratio::Float64 = 1
+abstract type DatasetType end
+
+@option "Nsf5_01" struct Nsf5_01 <: DatasetType
+    ratio::Float64 = 1
+end
+
+@option "Nsf5_02" struct Nsf5_02 <: DatasetType
+    ratio::Float64 = 1
+end
+
+@option "Nsf5_05" struct Nsf5_05 <: DatasetType
+    ratio::Float64 = 1
+end
+
+@option struct DataConfig
+    dataset::Union{Nsf5_01,Nsf5_02,Nsf5_05}
 end
 
 function split_in_half(x, ratio)
@@ -22,12 +33,16 @@ function split_in_half(x, ratio)
     return inds[1:k÷2], inds[(k÷2+1):k]
 end
 
-function load(d::Dataset)
-    x_cover = load_hdf5(datadir("dataset", d.cover_file))
-    x_stego = load_hdf5(datadir("dataset", d.stego_file))
+get_path(::Nsf5_01) = datadir("dataset", "nsf5_0.1_jrm.h5")
+get_path(::Nsf5_02) = datadir("dataset", "nsf5_0.2_jrm.h5")
+get_path(::Nsf5_05) = datadir("dataset", "nsf5_0.5_jrm.h5")
 
-    i1_train, i1_test = split_in_half(x_cover, d.cover_ratio)
-    i2_train, i2_test = split_in_half(x_stego, d.stego_ratio)
+function load(d::D) where {D<:DataConfig}
+    x_cover = load_hdf5(datadir("dataset", "cover_jrm.h5"))
+    x_stego = load_hdf5(get_path(d))
+
+    i1_train, i1_test = split_in_half(x_cover, 1)
+    i2_train, i2_test = split_in_half(x_stego, d.ratio)
 
     xtrain = hcat(x_cover[:, i1_train], x_stego[:, i2_train])
     ytrain = reshape(1:size(xtrain, 2) .> length(i1_train), 1, :)
