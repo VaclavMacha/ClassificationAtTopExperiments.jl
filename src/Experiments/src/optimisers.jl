6ba3497d4da30a5142b184b3da0d28750c374be1
machava2 <machava2@login1.rci.cvut.cz>
2022-04-13 13:42:29 +0200

    Fixed optimiser

--- src/Experiments/src/optimisers.jl
+++ src/Experiments/src/optimisers.jl
@@ -1,11 +1,11 @@
-@option "optimiser" struct Optimiser
-    type::Symbol = :Descent
+@option struct OptConfig
+    type::String = "Descent"
     eta::Real = 0.01
     decay_every::Int = 1
     decay_step::Real = 1
     decay_min::Real = 1e-6
 end
 
-materialize(o::Optimiser) = materialize(Val(o.type), o)
-materialize(::Val{:Descent}, o::Optimiser) = Descent(o.eta)
-materialize(::Val{:ADAM}, o::Optimiser) = ADAM(o.eta)
+materialize(o::OptConfig) = materialize(Val(Symbol(o.type)), o)
+materialize(::Val{:Descent}, o::OptConfig) = Descent(o.eta)
+materialize(::Val{:ADAM}, o::OptConfig) = ADAM(o.eta)
