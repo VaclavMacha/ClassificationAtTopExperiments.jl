783de9fea3baa8e00701a28aa1a5838a3523eace
machava2 <machava2@login1.rci.cvut.cz>
2022-04-13 13:42:03 +0200

    Added surrogates and thresholds

--- src/Experiments/src/losses.jl
+++ src/Experiments/src/losses.jl
@@ -13,18 +13,47 @@ function loss(o::CrossEntropy, y, s::AbstractArray{T}, pars) where {T}
     return T(o.λ) * sum(sqsum, pars) + logitbinarycrossentropy(s, y; agg)
 end
 
-@option "AATP" struct AATP <: LossType
-    λ::Float32 = 0
-    τ::Float32 = 0.01
-    l::Function = hinge
+# surrogates
+@option "Hinge" struct Hinge
     ϑ::Float32 = 1
 end
 
+materialize(l::Hinge, ϑ=l.ϑ) = x -> hinge(x, ϑ)
+
+@option "Quadratic" struct Quadratic
+    ϑ::Float32 = 1
+end
+
+materialize(l::Quadratic, ϑ=l.ϑ) = x -> quadratic(x, ϑ)
+
+# thresholds
+@option "PatMatNP" struct PatMatType
+    τ::Float32 = 0.01
+end
+
+materialize(t::PatMatType, l) = PatMatNP(t.τ, l)
+
+@option "TopPush" struct TopPushType end
+materialize(::TopPushType, l) = TopPush(l)
+
+# loss
+@option "AATP" struct AATP <: LossType
+    λ::Float32 = 0
+    surrogate::Union{Hinge,Quadratic} = Hinge()
+    threshold::Union{PatMatType,TopPushType} = PatMatType()
+end
+
 function loss(o::AATP, y, s::AbstractArray{T}, pars) where {T}
-    t = threshold(PatMatNP(o.τ, x -> o.l(x, o.ϑ)), y, s)
-    return T(o.λ) * sum(sqsum, pars) + fnr(y, s, t, o.l)
+    l1 = materialize(o.surrogate, 1)
+    l2 = materialize(o.surrogate)
+    t_type = materialize(o.threshold, l2)
+    t = threshold(t_type, y, s)
+    return T(o.λ) * sum(sqsum, pars) + fnr(y, s, t, l1)
 end
 
 @option struct LossConfig
     loss::Union{CrossEntropy,AATP}
 end
+
+loss(o::LossConfig, x, y, model, pars) = loss(o.loss, x, y, model, pars)
+loss(o::LossConfig, y, s, pars) = loss(o.loss, y, s, pars)
