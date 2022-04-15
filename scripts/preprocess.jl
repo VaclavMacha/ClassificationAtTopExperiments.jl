a45f52f5937d444e366bf3a2544b284fe3e96649
machava2 <machava2@login1.rci.cvut.cz>
2022-04-15 10:50:36 +0200

    Added full dataset

--- scripts/preprocess.jl
+++ scripts/preprocess.jl
@@ -26,7 +26,7 @@ function save_hdf5(
     # summary
     files = String[]
     for fdir in filedir.(dir, ids)
-        fls = filter(file -> contains(file, type), readdir(fdir; join = true))
+        fls = filter(file -> contains(file, type), readdir(fdir; join=true))
         if isempty(fls)
             @warn "File not found in $(fdir)"
         elseif length(fls) != 1
@@ -41,6 +41,7 @@ function save_hdf5(
     @info "# samples: $(n)"
 
     # save as h5
+    mkpath(savedir)
     h5open(joinpath(savedir, "$(type).h5"), "w") do fid
         data = create_dataset(fid, "data", datatype(Float32), dataspace(22510, n))
 
@@ -57,11 +58,19 @@ end
 
 dir = "/mnt/stego/flcc80-all-fea/"
 savedir = "/mnt/stego/alaska/"
-ids = setdiff(1:111, 6:10:116)
 
-save_hdf5(dir, ids, "nsf5_0.1_jrm"; savedir)
-save_hdf5(dir, ids, "nsf5_0.2_jrm"; savedir)
-save_hdf5(dir, ids, "nsf5_0.5_jrm"; savedir)
-save_hdf5(dir, ids, "cover_jrm"; savedir)
+# partial dataset
+ids = setdiff(1:111, 6:10:116)
+save_hdf5(dir, ids, "nsf5_0.1_jrm"; savedir=joinpath(savedir, "partial"))
+save_hdf5(dir, ids, "nsf5_0.2_jrm"; savedir=joinpath(savedir, "partial"))
+save_hdf5(dir, ids, "nsf5_0.5_jrm"; savedir=joinpath(savedir, "partial"))
+save_hdf5(dir, ids, "cover_jrm"; savedir=joinpath(savedir, "partial"))
+
+# full dataset
+ids = 1:47807
+save_hdf5(dir, ids, "nsf5_0.1_jrm"; savedir=joinpath(savedir, "full"))
+save_hdf5(dir, ids, "nsf5_0.2_jrm"; savedir=joinpath(savedir, "full"))
+save_hdf5(dir, ids, "nsf5_0.5_jrm"; savedir=joinpath(savedir, "full"))
+save_hdf5(dir, ids, "cover_jrm"; savedir=joinpath(savedir, "full"))
 
 @info "Finished"
