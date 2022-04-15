2c302d1c9124642ca1e407531428fb6700cb902b
machava2 <machava2@login1.rci.cvut.cz>
2022-04-15 10:47:49 +0200

    Updated config generation

--- scripts/generate_configs.jl
+++ scripts/generate_configs.jl
@@ -5,33 +5,25 @@ using Experiments
 
 configsdir(args...) = projectdir("configs", args...)
 
+full_dataset = true
+
 Dconfigs = [
-    DataConfig(type="Nsf5_01", stego_ratio=1),
-    DataConfig(type="Nsf5_01", stego_ratio=0.5),
-    DataConfig(type="Nsf5_01", stego_ratio=0.1),
-    DataConfig(type="Nsf5_02", stego_ratio=1),
-    DataConfig(type="Nsf5_02", stego_ratio=0.5),
-    DataConfig(type="Nsf5_02", stego_ratio=0.1),
-    DataConfig(type="Nsf5_05", stego_ratio=1),
-    DataConfig(type="Nsf5_05", stego_ratio=0.5),
-    DataConfig(type="Nsf5_05", stego_ratio=0.1),
+    DataConfig(; type="Nsf5_02", full_dataset, stego_ratio=1),
+    DataConfig(; type="Nsf5_02", full_dataset, stego_ratio=0.5),
+    DataConfig(; type="Nsf5_02", full_dataset, stego_ratio=0.1),
 ]
 
 Lconfigs = LossConfig.([
     CrossEntropy(ϵ=0.5),
-    CrossEntropy(ϵ=0.1),
-    CrossEntropy(ϵ=0.01),
-    CrossEntropy(ϵ=0.001),
+    CrossEntropy(ϵ=0.9),
+    CrossEntropy(ϵ=0.99),
+    CrossEntropy(ϵ=0.999),
     AATP(threshold=PatMatType(1e-3), surrogate=Hinge()),
     AATP(threshold=PatMatType(1e-4), surrogate=Hinge()),
     AATP(threshold=PatMatType(1e-5), surrogate=Hinge()),
-    AATP(threshold=PatMatType(1e-3), surrogate=Quadratic()),
-    AATP(threshold=PatMatType(1e-4), surrogate=Quadratic()),
-    AATP(threshold=PatMatType(1e-5), surrogate=Quadratic()),
 ])
 Oconfigs = [
     OptConfig(type="ADAM", eta=1e-2),
-    OptConfig(type="ADAM", eta=1e-3),
 ]
 Mconfig = ModelConfig(Linear())
 Tconfig = TrainConfig(iters=1000, checkpoint_every=100)
@@ -40,7 +32,7 @@ i = 0
 for Dconfig in Dconfigs, Lconfig in Lconfigs, Oconfig in Oconfigs
     i += 1
     d = make_dict(Lconfig, Mconfig, Dconfig, Oconfig, Tconfig)
-    path = configsdir("config_$(lpad(i, 4, "0")).yaml")
+    path = configsdir(ifelse(full_dataset, "full", "partial"), "config_$(lpad(i, 4, "0")).yaml")
     mkpath(dirname(path))
     save_config(path, d)
 end
