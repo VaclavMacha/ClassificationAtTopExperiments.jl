98fba3e09fa2c3fef38c1499f1284c2db541f48f
machava2 <machava2@login1.rci.cvut.cz>
2022-04-17 10:47:49 +0200

    Added lazy split

--- src/Experiments/src/datasets.jl
+++ src/Experiments/src/datasets.jl
@@ -9,13 +9,19 @@ function load_hdf5(path::AbstractString)
     end
 end
 
+function hdf5_size(path::AbstractString)
+    return h5open(path, "r") do fid
+        size(fid["data"])
+    end
+end
+
 @option struct DataConfig
     type::String = "Nsf5_01"
     full_dataset::Bool = false
-    at_train::Float32 = 0.45
-    at_valid::Float32 = 0.05
-    cover_ratio::Float32 = 1
-    stego_ratio::Float32 = 1
+    at_train::Float64 = 0.45
+    at_valid::Float64 = 0.05
+    cover_ratio::Float64 = 1
+    stego_ratio::Float64 = 1
 end
 
 function Base.string(d::DataConfig)
@@ -33,37 +39,42 @@ function split_data(n, at, ratio)
     return train, valid, test
 end
 
-function get_path(d::DataConfig)
-    return get_path(Val(Symbol(d.type)), ifelse(d.full_dataset, "full","partial"))
+function get_path(d::DataConfig; cover::Bool = false)
+    type = cover ? :Cover : Symbol(d.type)
+    return get_path(Val(type), ifelse(d.full_dataset, "full","partial"))
 end
+get_path(::Val{:Cover}, dir) = datadir("dataset", dir, "cover_jrm.h5")
 get_path(::Val{:Nsf5_01}, dir) = datadir("dataset", dir, "nsf5_0.1_jrm.h5")
 get_path(::Val{:Nsf5_02}, dir) = datadir("dataset", dir, "nsf5_0.2_jrm.h5")
 get_path(::Val{:Nsf5_05}, dir) = datadir("dataset", dir, "nsf5_0.5_jrm.h5")
 
 function load(d::DataConfig)
-    x_cover = load_hdf5(datadir(
-        "dataset",
-        ifelse(d.full_dataset, "full","partial"),
-        "cover_jrm.h5")
-    )
-    x_stego = load_hdf5(get_path(d))
+    file_cover = get_path(d; cover=true)
+    file_stego = get_path(d)
 
+    s_cover = hdf5_size(file_cover)
+    s_stego = hdf5_size(file_stego)
+    k = s_cover[1]
+    n = s_cover[2] + s_stego[2]
+
+    x = Array{Float32}(undef, k, n)
+    x[:, 1:s_cover[2]] = load_hdf5(file_cover)
+    x[:, (s_cover[2]+1):end] = load_hdf5(file_stego)
+    y = reshape((1:n) .> s_cover[2], 1, :)
+
+    # split
     at = (d.at_train, d.at_valid)
-    i1_train, i1_valid, i1_test = split_data(size(x_cover, 2), at, d.cover_ratio)
-    i2_train, i2_valid, i2_test = split_data(size(x_stego, 2), at, d.stego_ratio)
+    i1_train, i1_valid, i1_test = split_data(s_cover[2], at, d.cover_ratio)
+    i2_train, i2_valid, i2_test = split_data(s_stego[2], at, d.stego_ratio)
 
-    train = (
-        hcat(x_cover[:, i1_train], x_stego[:, i2_train]),
-        reshape(1:(length(i1_train)+length(i2_train)) .> length(i1_train), 1, :),
-    )
-    valid = (
-        hcat(x_cover[:, i1_valid], x_stego[:, i2_valid]),
-        reshape(1:(length(i1_valid)+length(i2_valid)) .> length(i1_valid), 1, :),
-    )
-    test = (
-        hcat(x_cover[:, i1_test], x_stego[:, i2_test]),
-        reshape(1:(length(i1_test)+length(i2_test)) .> length(i1_test), 1, :),
-    )
+    i_train = vcat(i1_train, i2_train .+ s_stego[2])
+    i_valid = vcat(i1_valid, i2_valid .+ s_stego[2])
+    i_test = vcat(i1_test, i2_test .+ s_stego[2])
+
+    train = @views (x[:, i_train], y[:, i_train])
+    valid = @views (x[:, i_valid], y[:, i_valid])
+    test = @views (x[:, i_test], y[:, i_test])
+
     @info """
     Dataset: all (cover/stego)
     ⋅ Train: $(length(train[2])) ($(sum(.~train[2]))/$(sum(train[2])))
