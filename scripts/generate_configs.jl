606914c63f89c74c13f84122bd582d6d06cf31b9
machava2 <machava2@login1.rci.cvut.cz>
2022-04-21 09:44:39 +0200

    Updated generate_config script

--- scripts/generate_configs.jl
+++ scripts/generate_configs.jl
@@ -5,34 +5,50 @@ using Experiments
 
 configsdir(args...) = projectdir("configs", args...)
 
-full_dataset = true
-
-Dconfigs = [
-    DataConfig(; type="Nsf5_02", full_dataset, stego_ratio=1),
-    DataConfig(; type="Nsf5_02", full_dataset, stego_ratio=0.5),
-    DataConfig(; type="Nsf5_02", full_dataset, stego_ratio=0.1),
-]
-
 Lconfigs = LossConfig.([
+    AATP(threshold=PatMatType(1e-3), surrogate=Hinge()),
+    AATP(threshold=PatMatType(1e-4), surrogate=Hinge()),
+    AATP(threshold=PatMatType(1e-5), surrogate=Hinge()),
     CrossEntropy(ϵ=0.5),
     CrossEntropy(ϵ=0.9),
     CrossEntropy(ϵ=0.99),
     CrossEntropy(ϵ=0.999),
-    AATP(threshold=PatMatType(1e-3), surrogate=Hinge()),
-    AATP(threshold=PatMatType(1e-4), surrogate=Hinge()),
-    AATP(threshold=PatMatType(1e-5), surrogate=Hinge()),
 ])
 Oconfigs = [
     OptConfig(type="ADAM", eta=1e-2),
 ]
 Mconfig = ModelConfig(Linear())
-Tconfig = TrainConfig(iters=1000, checkpoint_every=100)
+
+# Partial data
+Dconfigs = [
+    DataConfig(; type="Nsf5_02", false, stego_ratio=1),
+    DataConfig(; type="Nsf5_02", false, stego_ratio=0.5),
+    DataConfig(; type="Nsf5_02", false, stego_ratio=0.1),
+]
+Tconfig_partial = TrainConfig(iters=1000, checkpoint_every=100)
+
+i = 0
+for Dconfig in Dconfigs, Lconfig in Lconfigs, Oconfig in Oconfigs
+    i += 1
+    d = make_dict(Lconfig, Mconfig, Dconfig, Oconfig, Tconfig_partial)
+    path = configsdir("partial", "config_$(lpad(i, 4, "0")).yaml")
+    mkpath(dirname(path))
+    save_config(path, d)
+end
+
+# Full data
+Dconfigs = [
+    DataConfig(; type="Nsf5_02", true, stego_ratio=1),
+    DataConfig(; type="Nsf5_02", true, stego_ratio=0.5),
+    DataConfig(; type="Nsf5_02", true, stego_ratio=0.1),
+]
+Tconfig_full = TrainConfig(iters=100, checkpoint_every=5)
 
 i = 0
 for Dconfig in Dconfigs, Lconfig in Lconfigs, Oconfig in Oconfigs
     i += 1
     d = make_dict(Lconfig, Mconfig, Dconfig, Oconfig, Tconfig)
-    path = configsdir(ifelse(full_dataset, "full", "partial"), "config_$(lpad(i, 4, "0")).yaml")
+    path = configsdir("full", "config_$(lpad(i, 4, "0")).yaml")
     mkpath(dirname(path))
     save_config(path, d)
 end
