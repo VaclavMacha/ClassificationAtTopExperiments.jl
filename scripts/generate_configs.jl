58497080ffa1281abe7b9da42d4df808c194dbf3
machava2 <machava2@login1.rci.cvut.cz>
2022-04-26 09:16:14 +0200

    Fixed generate_configs script

--- scripts/generate_configs.jl
+++ scripts/generate_configs.jl
@@ -21,9 +21,9 @@ Mconfig = ModelConfig(Linear())
 
 # Partial data
 Dconfigs = [
-    DataConfig(; type="Nsf5_02", false, stego_ratio=1),
-    DataConfig(; type="Nsf5_02", false, stego_ratio=0.5),
-    DataConfig(; type="Nsf5_02", false, stego_ratio=0.1),
+    DataConfig(; type="Nsf5_02", full_dataset = false, stego_ratio=1),
+    DataConfig(; type="Nsf5_02", full_dataset = false, stego_ratio=0.5),
+    DataConfig(; type="Nsf5_02", full_dataset = false, stego_ratio=0.1),
 ]
 Tconfig_partial = TrainConfig(iters=1000, checkpoint_every=100)
 
@@ -38,16 +38,16 @@ end
 
 # Full data
 Dconfigs = [
-    DataConfig(; type="Nsf5_02", true, stego_ratio=1),
-    DataConfig(; type="Nsf5_02", true, stego_ratio=0.5),
-    DataConfig(; type="Nsf5_02", true, stego_ratio=0.1),
+    DataConfig(; type="Nsf5_02", full_dataset = true, stego_ratio=1),
+    DataConfig(; type="Nsf5_02", full_dataset = true, stego_ratio=0.5),
+    DataConfig(; type="Nsf5_02", full_dataset = true, stego_ratio=0.1),
 ]
-Tconfig_full = TrainConfig(iters=100, checkpoint_every=5)
+Tconfig_full = TrainConfig(iters=500, checkpoint_every=10)
 
 i = 0
 for Dconfig in Dconfigs, Lconfig in Lconfigs, Oconfig in Oconfigs
     i += 1
-    d = make_dict(Lconfig, Mconfig, Dconfig, Oconfig, Tconfig)
+    d = make_dict(Lconfig, Mconfig, Dconfig, Oconfig, Tconfig_full)
     path = configsdir("full", "config_$(lpad(i, 4, "0")).yaml")
     mkpath(dirname(path))
     save_config(path, d)
