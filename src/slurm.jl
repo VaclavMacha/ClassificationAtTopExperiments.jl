19bd03517e8017a17393c9dc60fa2c363f124675
Vaclav Macha <vaclav.macha92@gmail.com>
2022-05-15 10:20:25 +0200

    Improved slurm to support lists

--- src/slurm.jl
+++ src/slurm.jl
@@ -4,15 +4,19 @@ export sbatch, sbatch_array
 
 slurm_cmd(key::Symbol) = Symbol(replace(string(key), "_" => "-"))
 
-parse_arg(arg::Pair) = "--$(slurm_cmd(arg[1]))=$(arg[2])"
-parse_arg(arg) = "$(arg)"
+slurm_val(key) = string(key)
+slurm_val(key::AbstractArray) = join(slurm_val.(key), ",")
+slurm_val(key::Tuple) = join(slurm_val.(key), ",")
+
+parse_arg(arg::Pair) = "--$(slurm_cmd(arg[1]))=$(slurm_val(arg[2]))"
+parse_arg(arg) = slurm_val(arg)
 parse_args(args) = [parse_arg(arg) for arg in args]
 
 function sbatch(
     file::AbstractString,
     args...;
-    logdir::AbstractString = dirname(file),
-    output::AbstractString = joinpath(logdir, "%A.log"),
+    logdir::AbstractString=dirname(file),
+    output::AbstractString=joinpath(logdir, "%A.log"),
     kwargs...
 )
     sbatch_args = parse_args(kwargs)
@@ -26,9 +30,9 @@ end
 function sbatch_array(
     file::AbstractString,
     dir::AbstractString;
-    zero_indexing::Bool = false,
-    logdir::AbstractString = dirname(file),
-    output::AbstractString = joinpath(logdir, "%A-%a.log"),
+    zero_indexing::Bool=false,
+    logdir::AbstractString=dirname(file),
+    output::AbstractString=joinpath(logdir, "%A-%a.log"),
     kwargs...
 )
     sbatch_args = parse_args(kwargs)
