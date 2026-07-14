5c43b8aede183e3729d4aa24528edcd051830d58
Vaclav Macha <vaclav.macha92@gmail.com>
2026-07-14 13:07:33 +0200

    Update run script

--- scripts/steganography_new/run.jl
+++ scripts/steganography_new/run.jl
@@ -27,8 +27,28 @@ for folder in joinpath.("Nsf5SmallSGD", readdir(configs_dir("Nsf5SmallSGD")))
         scriptsdir("run_model.jl"),
         configs_dir(folder);
         logdir=log_dir(folder),
-        partition="amd",
+        partition="amdextralong",
         cpus_per_task=2,
         mem="100G"
     ) |> run
 end
+
+for folder in joinpath.("JMiPODSmall", readdir(configs_dir("JMiPODSmall")))
+    sbatch_array(
+        scriptsdir("run_model.jl"),
+        configs_dir(folder);
+        logdir=log_dir(folder),
+        partition="amdgpulong",
+        gres="gpu:1",
+        cpus_per_task=16,
+        mem="50G"
+    ) |> run
+end
+
+sbatch(
+    projectdir("scripts", "steganography_new", "gradient_convergence.jl";
+    logdir=log_dir(),
+    partition="amd",
+    cpus_per_task=2,
+    mem="100G"
+) |> run
